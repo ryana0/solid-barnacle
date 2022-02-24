e930c7a4c2d1ee9e6e27c22ec4164648f35757af
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-02-23 21:23:13 -0800

    thing asoph

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ io.on('connection', (socket) => {
     })
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('go to http://localhost:3000/startPage/')
   empty = {
     users: []
